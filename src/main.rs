9bcfff9dce2f648bf307d65e1a93e11c13980081
kyle-tennison <kyletennison05@gmail.com>
2025-06-03 19:39:32 -0400

    feat: image load & mask conversion

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::{
-    path::Path,
-    sync::{LazyLock, mpsc},
-    thread,
+    path::{Path, PathBuf}, process::exit, sync::{mpsc, LazyLock}, thread
 };
 
 extern crate nalgebra as na;
@@ -10,22 +8,79 @@ mod display;
 mod numeric;
 mod poission;
 mod sim;
+mod preprocessor;
 
+use clap::Parser;
 use display::DisplayPacket;
 use indicatif::{ProgressBar, ProgressStyle};
 use na::DMatrix;
 use num_traits::Zero;
+use tracing::{Level, debug, error, info, warn};
+use tracing_subscriber::{self, fmt::format::FmtSpan};
 
 type ScalarField = DMatrix<f32>;
 type VectorField = [ScalarField; 2];
 
-static FRAMES_PATH: LazyLock<&Path> = LazyLock::new(|| Path::new("sim-frames"));
+static DEFAULT_FRAMES_PATH: LazyLock<&Path> = LazyLock::new(|| Path::new("sim-frames2"));
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    frames_dir: Option<String>,
+
+    #[arg(short, long)]
+    show_video: bool,
+
+    #[arg(help = "The path to a PNG image to use as the mask. White pixels will permit fluid flow, black pixels will be treated as solid.")]
+    mask_path: Option<String>
+}
 
 fn main() {
+    // setup logging
+    tracing_subscriber::fmt()
+        .with_span_events(FmtSpan::ENTER | FmtSpan::EXIT)
+        .with_ansi(true)
+        .with_max_level(Level::DEBUG)
+        .init();
+
+    let args = Args::parse();
+
+    let frames_path = args
+        .frames_dir
+        .and_then(|p| Some(PathBuf::from(p)))
+        .unwrap_or((*DEFAULT_FRAMES_PATH).into());
+
+    debug!("Using frames path {:?}", frames_path);
+    debug!("Showing video? {:?}", args.show_video);
+
+    let mask: DMatrix<bool>;
+
+    if let Some(mask_path) = args.mask_path {
+        info!("Loading mask path {}...", &mask_path);
+        mask = match preprocessor::mask_from_image(PathBuf::from(&mask_path).as_path()){
+            Ok(m) => m,
+            Err(err) => {
+                error!("Failed to load mask from provided image {}, because: {}", mask_path, err);
+                exit(1);
+            }
+        };
+        debug!("Sucessfully created mask from image");
+    }
+    else{
+        warn!("Using example shape mask.");
+        mask = sim::NewtonianSim::sample_shape_mask(200, 200);
+    }
+
+
+
     let (sender, receiver) = mpsc::channel();
 
-    thread::spawn(|| {
-        display::image_io_loop(receiver).unwrap();
+    let frames_path_tread = frames_path.clone();
+    thread::spawn(move || {
+        display::image_io_loop(receiver, &frames_path_tread).unwrap();
     });
 
     let simtime = 15.00;
@@ -34,7 +89,7 @@ fn main() {
         1.,
         0.002,
         (8., 0.),
-        &sim::NewtonianSim::sample_shape_mask(200, 200),
+        &mask,
         (5., 5.),
         simtime,
         1.,
@@ -77,5 +132,5 @@ fn main() {
 
     println!("fps: {}; t={}", fps, t_outer);
 
-    display::play_video(fps, *FRAMES_PATH).unwrap();
+    display::play_video(fps, frames_path.as_path()).unwrap();
 }
---
+++ src/preprocessor.rs
@@ -0,0 +1,27 @@
+use std::{error::Error, path::Path};
+use image::{GenericImageView, ImageReader, Pixel};
+use na::DMatrix;
+
+const THRESHOLD_LUMA: u8 = 127;
+
+pub fn mask_from_image(image: &Path) -> Result<DMatrix<bool>, Box<dyn Error>>{
+
+
+    let image = ImageReader::open(image)?.decode()?;
+
+    let (nrows, ncols) = (image.height(), image.width());
+
+    let mut mask: DMatrix<bool> = DMatrix::from_element(nrows as usize, ncols as usize, false);
+
+    // load mask
+    image
+        .pixels()
+        .into_iter()
+        .for_each(|(x, y, color)| {
+            *(mask.index_mut((y as usize, x as usize))) = color.to_luma().0[0] < THRESHOLD_LUMA
+        });
+
+
+    Ok(mask)
+
+}
